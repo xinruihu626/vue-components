ad1a1a91071e8194c74145184d047daeeb18f51c
xinruihu626 <2143608989@qq.com>
2025-08-04 15:31:05 +0800

    完成全部组件编写

---
+++ assets/index-BwDNFk-V.js
@@ -0,0 +1,1 @@
+import{d as a,f as s,u as c,b as r,o as t}from"./index-DmvcB-pk.js";const p=a({__name:"index",setup(_){let o=e=>{console.log(e)};return(e,l)=>{const n=r("m-choose-area");return t(),s(n,{onChange:c(o)},null,8,["onChange"])}}});export{p as default};
